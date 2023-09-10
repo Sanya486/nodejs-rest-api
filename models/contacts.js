e73356b6d0ed0b919966e185311b4f9e73a7c1c6
Sanya486 <120394126+Sanya486@users.noreply.github.com>
2023-09-10 03:48:14 +0300

    task 2 done

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,59 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const { nanoid } = require("nanoid");
+const path = require("path");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  const contactsJSON = await fs.readFile(contactsPath, "utf-8");
+  const parsedContact = JSON.parse(contactsJSON);
+  return parsedContact || null;
+};
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const contactsList = await listContacts();
+  const contact = contactsList.find((contact) => contact.id === contactId);
+  return contact || null;
+};
 
-const addContact = async (body) => {}
+const removeContact = async (contactId) => {
+  const contactsList = await listContacts();
+  const contact = contactsList.find((contact) => contact.id === contactId);
+  const filteredContacts = contactsList.filter(
+    (contact) => contact.id !== contactId
+  );
+  fs.writeFile(contactsPath, JSON.stringify(filteredContacts, null, 2));
+  return contact || null;
+};
 
-const updateContact = async (contactId, body) => {}
+const addContact = async (body) => {
+  const { name, email, phone } = body;
+  const contact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  const contactsList = await listContacts();
+  const modContacts = [...contactsList, contact];
+  fs.writeFile(contactsPath, JSON.stringify(modContacts, null, 2));
+  return contact || null;
+};
+
+const updateContact = async (contactId, body) => {
+  const contactsList = await listContacts();
+  const contact = contactsList.find((contact) => contact.id === contactId);
+  if (!contact) {
+    return null;
+  }
+  const filteredContacts = contactsList.filter(
+    (contact) => contact.id !== contactId
+  );
+  const changedContact = { ...contact, ...body };
+  const modContacts = [...filteredContacts, changedContact];
+  fs.writeFile(contactsPath, JSON.stringify(modContacts, null, 2));
+  return changedContact;
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +61,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
