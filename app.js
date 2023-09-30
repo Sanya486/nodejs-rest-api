dbc79f8557230eef74154a808b4e1d0faa4ff9a3
Sanya486 <sanyok486@gmail.com>
2023-09-30 15:24:41 +0300

    mvp done

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ require("dotenv").config();
 
 const contactsRouter = require("./routes/api/contacts");
 const usersRouter = require("./routes/users");
-const { authenticate } = require("./middlewares");
 
 const app = express();
 
@@ -14,6 +13,7 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'))
 
 app.use("/api/contacts", contactsRouter);
 app.use("/users", usersRouter);
--- routes/users.js
+++ routes/users.js
@@ -1,38 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const user = require("../controllers/users");
-const {validateBody, authenticate} = require("../middlewares");
+const { validateBody, authenticate } = require("../middlewares");
 
 const validation = require("../utils/validation");
+const { upload } = require("../middlewares/upload");
 
 router.post(
   "/register",
-  validateBody(validation.registerSchema), 
+  validateBody(validation.registerSchema),
   user.register
 );
-
-router.post(
-  "/login",
-  validateBody(validation.loginSchema), 
-  user.login
-);
-
-router.post(
-  "/logout",
+router.post("/login", validateBody(validation.loginSchema), user.login);
+router.post("/logout", authenticate, user.logout);
+router.post("/current", authenticate, user.current);
+router.patch(
+  "/",
   authenticate,
-  user.logout
-);
-router.post(
-  "/current",
-  authenticate,
-  user.current
-);
-
-router.patch("/", authenticate, validateBody(validation.subscriptionSchema), user.updateSubscription);
-
-
-
-
-
-
-module.exports = router;
+  validateBody(validation.subscriptionSchema),
+  user.updateSubscription
+  );
+router.patch("/avatars", authenticate, upload.single('avatar'), user.updateAvatar);
+  
+  module.exports = router;
